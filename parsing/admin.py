08657f6afef225bb3875cffc67bdba67738ddf1b
Павел <paul-36@mail.ru>
2021-10-02 21:19:00 +0300

    Project has been rewritten with POOP, everything works.

--- parsing/admin.py
+++ parsing/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MarketPlace, Apartment
+
+admin.site.register(MarketPlace)
+admin.site.register(Apartment)
--- parsing/models.py
+++ parsing/models.py
@@ -2,42 +2,44 @@ from django.db import models
 
 
 class MarketPlace(models.Model):
-    name = models.CharField(
-        max_length=255, verbose_name='Имя площадки объявлений')
-    url = models.URLField(verbose_name='Ссылка на площадку')
-    main_block_tag = models.CharField(
-        max_length=255, verbose_name='Тэг главного блока')
+    name = models.CharField(max_length=255, verbose_name="Имя площадки объявлений")
+    url = models.URLField(verbose_name="Ссылка на площадку")
+    main_block_tag = models.CharField(max_length=255, verbose_name="Тэг главного блока")
     main_block_class_name = models.CharField(
-        max_length=255, verbose_name='Класс главного блока')
-    price_tag = models.CharField(
-        max_length=255, verbose_name='Тэг блока цены')
-    price_class = models.CharField(
-        max_length=255, verbose_name='Класс блока цены')
-    title_tag = models.CharField(
-        max_length=255, verbose_name='Класс блока заголовка')
-    title_class = models.CharField(
-        max_length=255, verbose_name='Класс блока заголовка')
-    url_tag = models.CharField(
-        max_length=255, verbose_name='Класс блока ссылки')
-    url_class = models.CharField(
-        max_length=255, verbose_name='Класс блока ссылки')
+        max_length=255, verbose_name="Класс главного блока"
+    )
+    price_tag = models.CharField(max_length=255, verbose_name="Тэг блока цены")
+    price_class = models.CharField(max_length=255, verbose_name="Класс блока цены")
+    title_tag = models.CharField(max_length=255, verbose_name="Класс блока заголовка")
+    title_class = models.CharField(max_length=255, verbose_name="Класс блока заголовка")
+    url_tag = models.CharField(max_length=255, verbose_name="Класс блока ссылки")
+    url_class = models.CharField(max_length=255, verbose_name="Класс блока ссылки")
     url_first_part = models.CharField(
-        max_length=255, verbose_name='Неизменяемая первая часть ссылки')
+        max_length=255, verbose_name="Неизменяемая первая часть ссылки"
+    )
 
     class Meta:
-        verbose_name = 'Площадка объявлений'
-        verbose_name_plural = 'Площадки объявлений'
+        verbose_name = "Площадка объявлений"
+        verbose_name_plural = "Площадки объявлений"
 
     def __srt__(self):
-        return f'Рекламная площадка {self.name}'
+        return f"Рекламная площадка {self.name}"
 
 
 class Apartment(models.Model):
-    name = models.CharField(
-        max_length=255, verbose_name='Заголовок объявления')
-    url = models.URLField(verbose_name='Ссылка на площадку')
-    price = models.PositiveIntegerField(
-        verbose_name='Стоимость квартиры'
+    name = models.CharField(max_length=255, verbose_name="Заголовок объявления")
+    url = models.URLField(verbose_name="Ссылка на площадку")
+    price = models.PositiveIntegerField(verbose_name="Стоимость квартиры")
+    total_area = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Общая площадь"
     )
-    total_area = models.DecimalField()
-    price_per_meter = models.DecimalField()
+    price_per_meter = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за метр"
+    )
+
+    class Meta:
+        verbose_name = "Объект недвижимости"
+        verbose_name_plural = "Объекты недвижимости"
+
+    def __srt__(self):
+        return f"Объект: {self.name}"
